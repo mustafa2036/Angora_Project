35e31e51504d94ed2fa87e30cd908548d9c2af72
mustafa2036 <missem704@gmail.com>
2022-08-01 13:02:15 +0200

    First Angora

---
+++ js/main.js
@@ -0,0 +1,34 @@
+let Navbar = document.querySelector('.navbar');
+
+window.onscroll = () => {
+    if(window.scrollY > 0){
+        Navbar.classList.add('active')
+    }
+    else{
+        Navbar.classList.remove('active')
+    }
+}
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    effect: 'fade',
+    speed:2000,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
+AOS.init();
+
+$(document).ready(function () {
+    $(".lds-ring").fadeOut(4000, () => {
+        $("#landing").fadeOut(1000 , () => {
+            $("body").css("overflow-y" , 'auto');
+        });
+    })
+});
